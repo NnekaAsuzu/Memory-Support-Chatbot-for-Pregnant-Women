d66d025c66b83a737b294411283aae0cf734e114
Nneka Asuzu <nnasuzu@gmail.com>
2024-03-08 21:53:32 -0500

    Delete streamlit_app.py

--- streamlit_app.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-
-def main():
-    st.title('Simple Streamlit App')
-    user_input = st.text_input('Enter some text:', 'Type here...')
-    st.write('You entered:', user_input)
-
-if __name__ == "__main__":
-    main()
